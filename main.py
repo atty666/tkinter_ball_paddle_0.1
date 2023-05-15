1c9b97ad4ae764e82f139bf979abab0933ed0d62
denys <supriadkin.d@gmail.com>
2023-05-15 16:05:21 +0300

    class Ball

--- main.py
+++ main.py
@@ -11,5 +11,24 @@ canvas = Canvas(tk, width=600, height=400, bd=0,
                 highlightthickness=0)
 canvas.pack()
 
-tk.update()
-tk.mainloop()
+
+class Ball:
+    def __init__(self, canvas, color):
+        self.canvas = canvas
+        self.id = canvas.create_oval(10, 10, 35, 35, fill=color)
+        self.canvas.move(self.id, 245, 100)
+
+    def draw(self):
+        pass
+
+
+ball = Ball(canvas, 'blue')
+
+while True:
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+
+
+# tk.update()
+# tk.mainloop()
