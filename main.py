0d1cc4227da2edeabb0676a90863b4f4a20234f5
denys <supriadkin.d@gmail.com>
2023-05-27 20:45:05 +0300

    updated

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ class Ball:
     def __init__(self, canvas, rocket):
         self.canvas = canvas
         self.rocket = rocket
-        self.colors = ['yellow']
+        self.colors = 'yellow'
         self.color_index = 0
         self.id = canvas.create_oval(10, 10, 35, 35,
-                                     fill=self.colors[self.color_index])
+                                     fill=self.colors)
         self.canvas.move(self.id, 245, 100)
         starts = [-3, -2, -1, 1, 2, 3]
         random.shuffle(starts)
@@ -18,6 +18,7 @@ class Ball:
         self.y = -3
         self.canvas_height = self.canvas.winfo_height()
         self.canvas_width = self.canvas.winfo_width()
+        self.hit_bottom = False
 
     def hit_rocket(self, pos):
         rocket_pos = self.canvas.coords(self.rocket.id)
@@ -30,9 +31,9 @@ class Ball:
         self.canvas.move(self.id, self.x, self.y)
         pos = self.canvas.coords(self.id)
         if pos[1] <= 0:
-            self.y = 1
+            self.y = 3
         if pos[3] >= self.canvas_height:
-            self.y = -1
+            self.hit_bottom = True
         if self.hit_rocket(pos) == True:
             self.y = -3
         if pos[0] <= 0:
@@ -40,16 +41,12 @@ class Ball:
         if pos[2] >= self.canvas_width:
             self.x = -3
 
-        #print(self.canvas.coords(self.id))      #enable for view ball coords
-
-    def change_color(self):
-        self.color_index = (self.color_index + 1) % len(self.colors)
-        self.canvas.itemconfig(self.id,
-                               fill=self.colors[self.color_index])
+        # print(self.canvas.coords(self.id))      #enable for view ball coords
 
 
 class Rocket:
     def __init__(self, canvas, color):
+        self.started = False
         self.canvas = canvas
         self.id = canvas.create_rectangle(0, 0, 100, 10,
                                           fill=color)
@@ -58,6 +55,7 @@ class Rocket:
         self.canvas_width = self.canvas.winfo_width()
         self.canvas.bind_all("<KeyPress-Left>", self.turn_left)
         self.canvas.bind_all("<KeyPress-Right>", self.turn_right)
+        self.canvas.bind_all("<Button-1>", self.start_game)
 
     def draw(self):
         self.canvas.move(self.id, self.x, 0)
@@ -67,13 +65,16 @@ class Rocket:
         elif pos[2] >= self.canvas_width:
             self.x = 0
 
-        print(self.canvas.coords(self.id))
+        # print(self.canvas.coords(self.id))
 
     def turn_left(self, evt):
         self.x = -2
 
     def turn_right(self, evt):
         self.x = 2
+
+    def start_game(self, evt):
+        self.started = True
 
 
 tk = Tk()
@@ -90,12 +91,10 @@ rocket = Rocket(canvas, 'blue')
 ball = Ball(canvas, rocket)
 
 while True:
-    ball.draw()
-    ball.change_color()
-    rocket.draw()
+    if not ball.hit_bottom and rocket.started:
+        ball.draw()
+        rocket.draw()
+
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
-
-# tk.update()
-# tk.mainloop()
